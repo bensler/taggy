cf4471efd8e1adf6da0665d719850e668fe729bd
Thomas Bensler <thomas.bensler@procilon.de>
2024-10-23 08:55:26 +0200

    dealing with empty db and unused code rm'ed

--- src/main/java/com/bensler/taggy/persist/DbConnector.java
+++ src/main/java/com/bensler/taggy/persist/DbConnector.java
@@ -1,15 +1,10 @@
 package com.bensler.taggy.persist;
 
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
+import java.io.File;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.logging.Logger;
-
-import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.output.MigrateResult;
@@ -30,19 +25,24 @@ public class DbConnector {
     Class<?>[] entityClasses, Consumer<Configuration> configManipulator
   ) {
     dbUrl = pDbUrl;
+    // first start on an non existing data dir
+    final File dbFile = new File(new LinkedList<>(Arrays.asList(dbUrl.split(":"))).getLast());
+    if (!dbFile.exists()) {
+      dbFile.getParentFile().mkdirs();
+    }
     migrationPath = pMigrationPath;
     final Configuration hbnConfig = new Configuration();
 
     Optional.ofNullable(configManipulator).ifPresent(pConfigManipulator -> pConfigManipulator.accept(hbnConfig));
     Arrays.stream(entityClasses)
       .map(clazz -> clazz.getName().replace('.', '/') + ".hbm.xml")
-      .forEach((String resourceName) -> hbnConfig.addResource(resourceName));
+      .forEach(hbnConfig::addResource);
     hbnConfig
       .setProperty("hibernate.transform_hbm_xml.enabled",     Boolean.TRUE.toString())
       .setProperty("hibernate.connection.driver_class",       driverClass.getName())
       .setProperty("hibernate.connection.url",                dbUrl)
       .setProperty("hibernate.show_sql",                      "true")
-      .setProperty("hibernate.connection.pool_size",          Integer.valueOf(1).toString());
+      .setProperty("hibernate.connection.pool_size",          Integer.toString(1));
     sessionFactory = hbnConfig.buildSessionFactory();
   }
 
@@ -54,65 +54,7 @@ public class DbConnector {
   }
 
   public MigrateResult performFlywayMigration() {
-    new DataSource() {
-
-      @Override
-      public <T> T unwrap(Class<T> iface) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
-      }
-
-      @Override
-      public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public void setLoginTimeout(int seconds) throws SQLException {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void setLogWriter(PrintWriter out) throws SQLException {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public int getLoginTimeout() throws SQLException {
-        // TODO Auto-generated method stub
-        return 0;
-      }
-
-      @Override
-      public PrintWriter getLogWriter() throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public Connection getConnection(String username, String password)
-          throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public Connection getConnection() throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-    };
-    return Flyway.configure().locations(migrationPath)
-        .dataSource(dbUrl, "", "").load().migrate();
+    return Flyway.configure().locations(migrationPath).dataSource(dbUrl, "", "").load().migrate();
   }
 
 }
