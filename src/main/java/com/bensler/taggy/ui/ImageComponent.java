c72dd1bba44ba3d54e0d14c3d0302ff8c7550b4d
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-11-04 20:38:26 +0100

    img centered in ImageComponent now

--- src/main/java/com/bensler/taggy/ui/ImageComponent.java
+++ src/main/java/com/bensler/taggy/ui/ImageComponent.java
@@ -10,13 +10,17 @@ import javax.swing.JComponent;
 public class ImageComponent extends JComponent {
 
   private BufferedImage img_;
+  private Image drawImg_;
+  private Dimension drawImgCompSize_;
 
   public ImageComponent() {
     img_ = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+    drawImgCompSize_ = new Dimension(-1, -1);
   }
 
   public void setImage(BufferedImage img) {
     img_ = img;
+    drawImgCompSize_ = new Dimension(-1, -1);
     revalidate();
     repaint();
   }
@@ -30,17 +34,25 @@ public class ImageComponent extends JComponent {
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     final Dimension size = getSize();
-    final double widthRatio = (img_.getWidth() / size.getWidth());
-    final double heightRatio = (img_.getHeight() / size.getHeight());
-    final Image drawImage;
 
-    if (widthRatio > heightRatio) {
-      drawImage = img_.getScaledInstance(size.width, -1, 0);
-    } else {
-      drawImage = img_.getScaledInstance(-1, size.height, 0);
+    if (!size.equals(drawImgCompSize_)) {
+      final double widthRatio = (img_.getWidth() / size.getWidth());
+      final double heightRatio = (img_.getHeight() / size.getHeight());
+
+      if (widthRatio > heightRatio) {
+        drawImg_ = img_.getScaledInstance(size.width, -1, 0);
+      } else {
+        drawImg_ = img_.getScaledInstance(-1, size.height, 0);
+      }
+      drawImgCompSize_ = size;
     }
 
-    g.drawImage(drawImage, 0,  0, this);
+    g.drawImage(
+      drawImg_,
+      (size.width - drawImg_.getWidth(null)) / 2,
+      (size.height - drawImg_.getHeight(null)) / 2,
+      this
+    );
   }
 
 
