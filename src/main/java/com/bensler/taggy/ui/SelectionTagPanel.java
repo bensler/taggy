95079af09281f785138ca5b977036779448a46a0
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2024-12-07 22:24:33 +0100

    make use of Hierarchical.toPath

--- src/main/java/com/bensler/taggy/ui/SelectionTagPanel.java
+++ src/main/java/com/bensler/taggy/ui/SelectionTagPanel.java
@@ -1,13 +1,13 @@
 package com.bensler.taggy.ui;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.swing.JScrollPane;
 
 import com.bensler.decaf.swing.tree.EntityTree;
+import com.bensler.decaf.util.tree.Hierarchical;
 import com.bensler.decaf.util.tree.Hierarchy;
 import com.bensler.taggy.persist.Blob;
 import com.bensler.taggy.persist.Tag;
@@ -22,16 +22,13 @@ public class SelectionTagPanel {
   }
 
   public void setData(Collection<Blob> blobs) {
-    final Set<Tag> assignedTags = blobs.stream()
+    final Set<Tag> allTags = blobs.stream()
         .map(Blob::getTags)
         .flatMap(Set::stream)
+        .distinct()
+        .flatMap(tag -> Hierarchical.toPath(tag).stream())
+        .distinct()
         .collect(Collectors.toSet());
-    final Set<Tag> allTags = new HashSet<>(assignedTags);
-    assignedTags.forEach(tag -> {
-      while (((tag = tag.getParent()) != null) && (!allTags.contains(tag))) {
-        allTags.add(tag);
-      }
-    });
 
     tagTree_.setData(new Hierarchy<>(allTags));
     tagTree_.expandCollapseAll(true);
