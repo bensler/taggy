f5d501bf6ac7797b594ace09360b89d5b432944a
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2025-05-30 22:38:51 +0200

    focus action added

--- src/main/java/com/bensler/taggy/ui/MainFrame.java
+++ src/main/java/com/bensler/taggy/ui/MainFrame.java
@@ -116,7 +116,7 @@ public class MainFrame {
       "3dlu, f:p, 3dlu, f:p:g, 3dlu, f:p, 3dlu"
     ));
 
-    final SelectionTagPanel selectionTagPanel = new SelectionTagPanel();
+    final SelectionTagPanel selectionTagPanel = new SelectionTagPanel(this);
     final JPanel toolbar = new JPanel(new FormLayout("f:p, 3dlu:g", "f:p"));
     toolbar.add(app_.getImportCtrl().getImportAction().createToolbarButton(), new CellConstraints(1, 1));
     mainPanel.add(toolbar, new CellConstraints(2, 2));
@@ -226,6 +226,10 @@ public class MainFrame {
         tagTree_.select(List.of());
       }
     }
+  }
+
+  void selectTag(Tag tag) {
+    tagTree_.select(tag);
   }
 
   public void show() {
--- src/main/java/com/bensler/taggy/ui/SelectionTagPanel.java
+++ src/main/java/com/bensler/taggy/ui/SelectionTagPanel.java
@@ -1,11 +1,19 @@
 package com.bensler.taggy.ui;
 
+import static com.bensler.decaf.swing.action.ActionState.ENABLED;
+import static com.bensler.decaf.swing.action.ActionState.HIDDEN;
+
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.swing.JScrollPane;
 
+import com.bensler.decaf.swing.action.ActionAppearance;
+import com.bensler.decaf.swing.action.ActionGroup;
+import com.bensler.decaf.swing.action.EntityAction;
+import com.bensler.decaf.swing.action.SingleEntityActionAdapter;
+import com.bensler.decaf.swing.action.SingleEntityFilter;
 import com.bensler.decaf.swing.tree.EntityTree;
 import com.bensler.decaf.util.tree.Hierarchical;
 import com.bensler.decaf.util.tree.Hierarchy;
@@ -16,19 +24,25 @@ public class SelectionTagPanel {
 
   final EntityTree<Tag> tagTree_;
 
-  public SelectionTagPanel() {
+  public SelectionTagPanel(MainFrame mainFrame) {
     tagTree_ = new EntityTree<>(MainFrame.TAG_NAME_VIEW);
     tagTree_.setVisibleRowCount(20, .5f);
+    final EntityAction<Tag> focusAction = new EntityAction<>(
+      new ActionAppearance(null, null, "Focus", null),
+      new SingleEntityFilter<>(HIDDEN, tag -> ENABLED),
+      new SingleEntityActionAdapter<>((source, tag) -> tag.ifPresent(mainFrame::selectTag))
+    );
+    tagTree_.setContextActions(new ActionGroup<>(focusAction));
   }
 
   public void setData(Collection<Blob> blobs) {
     final Set<Tag> allTags = blobs.stream()
-        .map(Blob::getTags)
-        .flatMap(Set::stream)
-        .distinct()
-        .flatMap(tag -> Hierarchical.toPath(tag).stream())
-        .distinct()
-        .collect(Collectors.toSet());
+      .map(Blob::getTags)
+      .flatMap(Set::stream)
+      .distinct()
+      .flatMap(tag -> Hierarchical.toPath(tag).stream())
+      .distinct()
+      .collect(Collectors.toSet());
 
     tagTree_.setData(new Hierarchy<>(allTags));
     tagTree_.expandCollapseAll(true);
