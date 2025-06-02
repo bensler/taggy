093273d5022560d06180ed192bb37175caa3ff7d
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2025-06-02 10:00:37 +0200

    AllTagsTreeFiltered is actually filtering now

--- src/main/java/com/bensler/taggy/ui/AllTagsTreeFiltered.java
+++ src/main/java/com/bensler/taggy/ui/AllTagsTreeFiltered.java
@@ -6,6 +6,7 @@ import static com.jgoodies.forms.layout.CellConstraints.DEFAULT;
 import static com.jgoodies.forms.layout.CellConstraints.FILL;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,6 +14,8 @@ import javax.swing.JTextField;
 
 import com.bensler.decaf.swing.tree.CheckboxTree;
 import com.bensler.decaf.swing.tree.CheckboxTree.CheckedListener;
+import com.bensler.decaf.util.tree.Hierarchical;
+import com.bensler.decaf.util.tree.Hierarchy;
 import com.bensler.taggy.App;
 import com.bensler.taggy.persist.Tag;
 import com.jgoodies.forms.layout.CellConstraints;
@@ -20,23 +23,37 @@ import com.jgoodies.forms.layout.FormLayout;
 
 public class AllTagsTreeFiltered {
 
-  private final CheckboxTree<Tag> allTags_;
+  private final CheckboxTree<Tag> tagTree_;
+  private final Set<Tag> allTags_;
   private final JPanel component_;
 
   public AllTagsTreeFiltered(CheckedListener<Tag> listener) {
     final App app = App.getApp();
-    allTags_ = new CheckboxTree<>(TAG_NAME_VIEW);
-    allTags_.setVisibleRowCount(20, 1);
-    app.getTagCtrl().setAllTags(allTags_);
-    allTags_.addCheckedListener(listener);
+    tagTree_ = new CheckboxTree<>(TAG_NAME_VIEW);
+    tagTree_.setVisibleRowCount(20, 1);
+    app.getTagCtrl().setAllTags(tagTree_);
+    allTags_ = tagTree_.getData().getMembers();
+    tagTree_.addCheckedListener(listener);
 
     final JTextField filterTf = new JTextField(5);
     component_ = new JPanel(new FormLayout("3dlu, p, 3dlu, f:p:g", "3dlu, p, 3dlu, f:p:g"));
     component_.add(new JLabel("Filter:"), new CellConstraints(2, 2));
     component_.add(filterTf, new CellConstraints(4, 2));
-    component_.add(allTags_.getScrollPane(), new CellConstraints(1, 4, 4, 1, FILL, DEFAULT));
+    component_.add(tagTree_.getScrollPane(), new CellConstraints(1, 4, 4, 1, FILL, DEFAULT));
 
-    addTextfieldListener(filterTf, str -> System.out.println("# " + str));
+    addTextfieldListener(filterTf, this::filterChanged);
+  }
+
+  private void filterChanged(String filterStr) {
+    final String matchStr = filterStr.toLowerCase();
+    final Hierarchy<Tag> filteredTags = new Hierarchy<>(allTags_.stream()
+      .filter(tag -> tag.getName().toLowerCase().contains(matchStr))
+      .flatMap(tag -> Hierarchical.toPath(tag).stream())
+      .distinct()
+      .collect(Collectors.toSet())
+    );
+
+    tagTree_.setData(filteredTags);
   }
 
   public JPanel getComponent() {
@@ -44,21 +61,22 @@ public class AllTagsTreeFiltered {
   }
 
   public Set<Tag> getCheckedNodes() {
-    return allTags_.getCheckedNodes();
+    return tagTree_.getCheckedNodes();
   }
 
   public void setCheckedNodes(Set<Tag> checkedTags) {
-    allTags_.setCheckedNodes(checkedTags);
+    tagTree_.setCheckedNodes(checkedTags);
   }
 
   public void expandCollapseAll(boolean expanded) {
-    allTags_.expandCollapseAll(expanded);
+    tagTree_.expandCollapseAll(expanded);
   }
 
   public void makeAllTagsVisible(Set<Tag> tags) {
-    allTags_.expandCollapseAll(false);
-    allTags_.setCheckedNodes(tags);
-    tags.forEach(tag -> allTags_.expandCollapse(tag, true));
+    setCheckedNodes(tags);
+    tagTree_.expandCollapseAll(false);
+    tagTree_.setCheckedNodes(tags);
+    tags.forEach(tag -> tagTree_.expandCollapse(tag, true));
   }
 
 }
