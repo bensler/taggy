d4ccbcac8b3d5399b78e90488622d724a3191a3f
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2025-11-18 14:32:32 +0100

    redundant code rm'ed

--- src/main/java/com/bensler/taggy/ui/TagUi.java
+++ src/main/java/com/bensler/taggy/ui/TagUi.java
@@ -2,6 +2,7 @@ package com.bensler.taggy.ui;
 
 import static com.bensler.decaf.swing.view.SimplePropertyGetter.createGetter;
 import static com.bensler.decaf.util.cmp.CollatorComparator.COLLATOR_COMPARATOR;
+import static com.bensler.taggy.persist.TagProperty.REPRESENTED_DATE;
 
 import java.util.List;
 
@@ -14,33 +15,29 @@ import com.bensler.decaf.swing.view.SimpleCellRenderer;
 import com.bensler.decaf.util.cmp.ComparableComparator;
 import com.bensler.decaf.util.cmp.ComparatorChain;
 import com.bensler.taggy.persist.Tag;
-import com.bensler.taggy.persist.TagProperty;
 
 public final class TagUi {
 
   public static final EntityPropertyComparator<Tag, String> NAME_COMPARATOR = new EntityPropertyComparator<>(Tag::getName, COLLATOR_COMPARATOR);
 
   public static final EntityPropertyComparator<Tag, String> DATE_COMPARATOR = new EntityPropertyComparator<>(
-    tag -> tag.getProperty(TagProperty.REPRESENTED_DATE), new ComparableComparator<>()
+    tag -> tag.getProperty(REPRESENTED_DATE), new ComparableComparator<>()
   );
 
-  public static final PropertyViewImpl<Tag, String> NAME_VIEW = new PropertyViewImpl<>(
-    new TagCellRenderer(), createGetter(Tag::getName, new ComparatorChain<>(List.of(DATE_COMPARATOR, NAME_COMPARATOR)))
-  );
-
-  public static final FilterOne<Tag> TIMELINE_TAG_FILTER = tag -> tag.containsProperty(TagProperty.REPRESENTED_DATE);
+  public static final FilterOne<Tag> TIMELINE_TAG_FILTER = tag -> tag.containsProperty(REPRESENTED_DATE);
 
   public static final FilterOne<Tag> TAG_FILTER = tag -> !TIMELINE_TAG_FILTER.matches(tag);
 
   public static final SimpleCellRenderer<Tag, String> CELL_RENDERER = new SimpleCellRenderer<>() {
-
     @Override
     protected Icon getIcon(Tag tag, String name) {
-      return (tag.getProperty(TagProperty.REPRESENTED_DATE) == null)
-        ? Icons.TAG_SIMPLE_13 : Icons.TIMELINE_13;
+      return (tag.containsProperty(REPRESENTED_DATE)) ? Icons.TAG_SIMPLE_13 : Icons.TIMELINE_13;
     }
-
   };
+
+  public static final PropertyViewImpl<Tag, String> NAME_VIEW = new PropertyViewImpl<>(
+    CELL_RENDERER, createGetter(Tag::getName, new ComparatorChain<>(List.of(DATE_COMPARATOR, NAME_COMPARATOR)))
+  );
 
   private TagUi() {}
 
